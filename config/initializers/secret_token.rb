3b3da364245d94a8ef49cdd69646dad249faf362
Thomas Dippel <thomas@dippel.dk>
2011-11-09 17:27:20 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PusherDemo::Application.config.secret_token = 'ecc7633813a389f1182a539d7deae3fa6049954c0fe1049786cad872901c7422f31cc0addf49c9e71a1d4f844bc151400c48e89c9437dc30a16e73987ebd9111'
