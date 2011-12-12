cdee3890990971d1642183147fddfdddf1059b2b
Thomas Dippel <thomas@dippel.dk>
2011-12-12 21:17:12 +0100

    Push "Total Messages Sent"

--- app/models/message_observer.rb
+++ app/models/message_observer.rb
@@ -2,5 +2,6 @@ class MessageObserver < ActiveRecord::Observer
   include ActionView::Helpers::TextHelper
 
   def after_create(message)
+    Pusher['broadcast'].trigger!('total_messages_changed', {:value => pluralize(Message.count, 'message', 'messages')})
   end
 end
