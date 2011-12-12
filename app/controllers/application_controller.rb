98ce0148bb1c8adb7395e7d82a379d152b32cf8e
Thomas Dippel <thomas@dippel.dk>
2011-12-12 20:30:54 +0100

    Helpers for getting count of unread messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user, :unread_messages_count
 
-  private
+  protected
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def unread_messages_count
+    @unread_messages_count ||= current_user ? current_user.received_messages.unread.count : 0
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  
+
   def bootstrap_flash_key_for(key)
     { :notice => "success", :alert => "error" }.fetch(key) { |key| key.to_s }
+  end
+
+  def messages_link(text, options = {})
+    link_to unread_messages_count > 0 ? "#{text} <span class=\"label important\">#{unread_messages_count}</span>".html_safe : text,
+            messages_path, options
   end
 
 end
